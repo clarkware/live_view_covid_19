741f53c0782b0a9d20e67dcc62ebf6351db89829
Mike Clark <mike@clarkware.com>
2020-03-17 18:14:58 -0600

    Set endpoint's production host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :covid_19, Covid19Web.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "powerful-beautiful-shrike.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
